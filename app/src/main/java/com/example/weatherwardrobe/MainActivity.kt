8eb867af7a1aac85151679840d21c7f5ee608a9d
azmoh09 <momoo362514@gmail.com>
2023-04-08 09:59:52 +0200

    add: make request with ok http

--- app/src/main/java/com/example/weatherwardrobe/MainActivity.kt
+++ app/src/main/java/com/example/weatherwardrobe/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.weatherwardrobe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.weatherwardrobe.databinding.ActivityMainBinding
 import okhttp3.*
 import java.io.IOException
@@ -25,16 +26,21 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun makeRequestWithOKHTTP() {
-        val request = Request.Builder()
-            .url("https://api.tomorrow.io/v4/timelines?apikey=i7fDcivw9N9Rm0CPUZjCKnBolxCfMnnM&location=40.75872069597532,-73.98529171943665&fields=temperature&timesteps=1h&units=metric")
+        val url = HttpUrl.Builder()
+            .scheme("http")
+            .host("api.weatherstack.com")
+            .addPathSegments("current")
+            .addQueryParameter("access_key", "6c499a638cccf2e9292fa288a60f8b10")
+            .addQueryParameter("query", "palestine")
             .build()
+
+        val request = Request.Builder().url(url).build()
         client.newCall(request).enqueue(object : Callback {
             override fun onFailure(call: Call, e: IOException) {
-
+                Log.d(MainActivity::class.java.name, "${e.message}")
             }
 
             override fun onResponse(call: Call, response: Response) {
-
                 runOnUiThread {
                     binding.textData.text = response.body?.string().toString()
                 }
